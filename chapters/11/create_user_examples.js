d19c1da570480deb6c1ad81242d9ff190805d413
dbierer <doug@unlikelysource.com>
2019-12-04 10:15:16 +0700

    2019-12-04 DB: updated sample data + added examples for chap 11

---
+++ chapters/11/create_user_examples.js
@@ -0,0 +1,46 @@
+// examples of creating users
+// NOTE: you can't run this code!  These are for reference only.
+
+// create a "super" user
+conn = new Mongo();
+db = conn.getDB("admin");
+superMan = {
+  user  : "superMan",
+  pwd   : "password",
+  roles : [ { role : "userAdminAnyDatabase", db : "admin" },
+              "readWriteAnyDatabase" ]
+}
+db.createUser(superMan);
+
+// set iteration to 10000
+db.adminCommand( { setParameter: 1, scramSHA256IterationCount: 10000 } );
+// create a user using SCRAM-SHA-256
+scramUser = {
+  user  : "scramUser",
+  pwd   : "password",
+  roles : [ { "readAnyDatabase" } ],
+  mechanisms: [ "SCRAM-SHA-256" ]
+}
+db.createUser(scramUser);
+// set iteration back to default
+db.adminCommand( { setParameter: 1, scramSHA256IterationCount: 15000 } );
+
+
+// create user with "read" rights on "biglittle"
+bgReader = {
+  user  : "bgReader",
+  pwd   : "password",
+  roles : [ { role:"read", db:"biglittle" } ],
+  mechanisms: [ "SCRAM-SHA-256" ]
+}
+db.createUser(bgReader);
+
+// create user with "readWriteAnyDatabase" rights
+use admin;
+rwAll = {
+  user  : "rwAll",
+  pwd   : "password",
+  roles : [ "readWriteAnyDatabase" ],
+  mechanisms: [ "SCRAM-SHA-256" ]
+}
+db.createUser(rwAll);
